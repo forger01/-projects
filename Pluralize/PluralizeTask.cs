05c9a1945f95a8b5f214d8c8445438acd6d86889
forger01 <banivashka@gmail.com>
2018-02-01 16:39:10 +0300

    projects on c#

---
+++ Pluralize/PluralizeTask.cs
@@ -0,0 +1,18 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+            if ((count % 10) == 1)
+                return "рубль";
+            else if (((count % 10) > 1) && (count % 10 < 5))
+                return "рубля";
+            else if (((count % 10) > 4) && (count % 10 < 10))
+                return "рублей";
+            else return "рублей";
+
+        }
+	}
+}
